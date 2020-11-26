3718f655c84012c26b128cca73e199e154495608
NickHellemans <60689153+NickHellemans@users.noreply.github.com>
2020-11-26 22:44:23 +0100

    Header component / index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,110 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import { COLORS } from "../constants.js"
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
+import {
+  Wrapper,
+  Image,
+  Artist,
+  BottomEdgeDown,
+  BottomEdgeUp,
+} from "./pageStyles/pageStyles"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const {
+    wpcontent: {
+      page: {
+        homeMeta: {
+          homePageDescription,
+          homePageFeaturedMovies,
+          homePageHeaderDescription,
+          homePageHeaderPicture,
+          homePageHeaderTitle,
+        },
+      },
+    },
+  } = useStaticQuery(graphql`
+    query {
+      wpcontent {
+        page(id: "home", idType: URI) {
+          homeMeta {
+            homePageDescription
+            homePageHeaderDescription
+            homePageHeaderTitle
+            homePageHeaderPicture {
+              altText
+              sourceUrl
+              imageFile {
+                childImageSharp {
+                  fluid(quality: 100) {
+                    ...GatsbyImageSharpFluid_withWebp
+                  }
+                }
+              }
+            }
+            homePageFeaturedMovies {
+              ... on WPGraphql_Movie {
+                id
+                movie {
+                  title
+                  poster {
+                    altText
+                    sourceUrl
+                    imageFile {
+                      childImageSharp {
+                        fluid(quality: 100, grayscale: true) {
+                          ...GatsbyImageSharpFluid_withWebp
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Wrapper>
+        <div className="banner">
+          <Image
+            fluid={homePageHeaderPicture.imageFile.childImageSharp.fluid}
+            altText={homePageHeaderPicture.altText}
+          />
+          <div className="inner-div">
+            <p className="header-title">{homePageHeaderTitle}</p>
+            <p className="header-description">{homePageHeaderDescription}</p>
+          </div>
+          <BottomEdgeDown color={COLORS.BLACK} />
+        </div>
+        <div className="description">
+          <p>{homePageDescription}</p>
+          <BottomEdgeUp color={COLORS.PRIMARY} />
+        </div>
+        <div className="artists">
+          <h2>Featured movies</h2>
+          <div className="artist-items">
+            {homePageFeaturedMovies.map(({ movie, slug }) => (
+              <Artist to={`/${slug}`}>
+                <Image
+                  fluid={movie.poster.imageFile.childImageSharp.fluid}
+                  altText={movie.poster.altText}
+                />
+                <div className="artist-info">
+                  <p>{movie.title}</p>
+                </div>
+              </Artist>
+            ))}
+          </div>
+        </div>
+      </Wrapper>
+    </Layout>
+  )
+}
 
 export default IndexPage
